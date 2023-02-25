2fa449cd345b4962d1fe78346b9590e09b9315e8
Deny <flutterlabz@gmail.com>
2023-02-25 11:16:13 +0700

    '.'

--- lib/core.dart
+++ lib/core.dart
@@ -5,6 +5,16 @@ it means you have to rename it to something more unique.
 */
 export 'package:hyper_ui/config.dart';
 export 'package:hyper_ui/core_package.dart';
+export 'package:hyper_ui/module/dashboard/controller/dashboard_controller.dart';
+export 'package:hyper_ui/module/dashboard/view/dashboard_view.dart';
+export 'package:hyper_ui/module/favorite/controller/favorite_controller.dart';
+export 'package:hyper_ui/module/favorite/view/favorite_view.dart';
+export 'package:hyper_ui/module/main_navigation/controller/main_navigation_controller.dart';
+export 'package:hyper_ui/module/main_navigation/view/main_navigation_view.dart';
+export 'package:hyper_ui/module/order/controller/order_controller.dart';
+export 'package:hyper_ui/module/order/view/order_view.dart';
+export 'package:hyper_ui/module/profile/controller/profile_controller.dart';
+export 'package:hyper_ui/module/profile/view/profile_view.dart';
 export 'package:hyper_ui/setup.dart';
 export 'package:hyper_ui/shared/theme/theme.dart';
 export 'package:hyper_ui/shared/theme/theme_config.dart';
