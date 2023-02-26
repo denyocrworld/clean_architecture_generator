00b08c2c54ff3a26c74ebf0a7d88edeac344a3a8
Deny <flutterlabz@gmail.com>
2023-02-26 22:47:02 +0700

    '.'

--- lib/main.dart
+++ lib/main.dart
@@ -2,50 +2,6 @@ import 'package:hyper_ui/core.dart';
 import 'package:flutter/material.dart';
 import 'package:hyper_ui/debug.dart';
 
-product() {}
-
-getUser() async {
-  var response = await Dio().get(
-    "https://reqres.in/api/users",
-    options: Options(
-      headers: {
-        "Content-Type": "application/json",
-      },
-    ),
-  );
-  Map obj = response.data;
-}
-
-addProduct() async {
-  var user = await getUser();
-
-  var response = await Dio().post(
-    "https://reqres.in/api/users",
-    options: Options(
-      headers: {
-        "Content-Type": "application/json",
-      },
-    ),
-    data: {
-      "name": "morpheus",
-      "job": "programmer",
-    },
-  );
-  Map obj = response.data;
-}
-
-deleteProduct() {}
-updateProduct() {}
-getProduct() {}
-
-dltProduct() {}
-updtPrdct() {}
-
-deleteProductOfCustomerFromDatabase() {}
-
-addProductHttpRequest() {}
-addProductLocalStorage() {}
-
 void main() async {
   await initialize();
 
